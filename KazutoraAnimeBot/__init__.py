2e96b4c86e5ffb5b36b224f662da6ea5f45c9bed
Aarukami <91074890+Aarukami@users.noreply.github.com>
2021-10-03 17:53:53 +0530

    Update __init__.py

--- KazutoraAnimeBot/__init__.py
+++ KazutoraAnimeBot/__init__.py
@@ -195,7 +195,7 @@ DEMONS = list(DEMONS)
 TIGERS = list(TIGERS)
 
 # Load at end to ensure all prev variables have been set
-from XForceSTB.modules.helper_funcs.handlers import (CustomCommandHandler,
+from KzautoraAnimeBot.modules.helper_funcs.handlers import (CustomCommandHandler,
                                                         CustomMessageHandler,
                                                         CustomRegexHandler)
 
